963cd7ffa3534fbae461b4d1569e939c10db2d52
Matthew O'Connor <Matthew@mbp>
2021-05-19 02:27:12 +0100

    'add contact form modal'

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -28,6 +28,60 @@ request.onload = function () {
 };
 request.send(body);
 
+var contactModal = document.getElementById('contactUs')
+var leaderboardModal = document.getElementById('leaderboard')
+
+contactModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+leaderboardModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+
+var form = document.getElementById("contact");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+      form.reset();
+      alert( "Success! we will be in touch shortly.");
+      document.getElementById("closeContactModal").click(); // Close the modal on success
+
+    }
+
+    function error() {
+      alert( "Oops! There was a problem.");
+    }
+
+    // handle the form submission event from formspree.io
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault(); // stop the standard form POST submit so we can stay on our page and display a response to the user.
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+
+
+  // helper function for sending an AJAX request from formspree.io
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+
+
 
 $(document).ready(function() {
   setTimeout(() => { $(".loader").hide();
